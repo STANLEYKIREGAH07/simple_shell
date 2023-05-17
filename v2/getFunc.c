e4d58368fb4ea0820915c3125e983c7752f63fe5
alijr2018 <alirami120@hotmail.fr>
2023-05-18 00:06:26 +0100

    it worked

---
+++ v2/getFunc.c
@@ -0,0 +1,74 @@
+#include "main.h"
+
+/**
+ * parse_input - function is to parse the input string into separate tokens. 
+ * @input: pointer to character.
+ * @tokens: pointer to an array of pointers to characters (double pointer).
+ * @tokens_num: pointer to integer
+ * Return: 
+*/
+
+void parse_input(char* input, char** tokens, int* tokens_num) 
+{
+    char* token;
+    char* a;
+
+    a = " \t\n\r\a";
+    token = strtok(input, a);
+
+    for (*tokens_num = 0;token != NULL && *tokens_num < MAX_NUM_TOKENS; (*tokens_num)++) 
+    {
+        tokens[*tokens_num] = token;
+        token = strtok(NULL, a);
+    }
+    tokens[*tokens_num] = NULL;
+}
+
+/**
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
+
+/**
+ * _print - print a string
+ * @str: pointer to string
+*/
+void _print(char *str)
+{
+    int i;
+
+    for (i = 0; str[i]; i++)
+    {
+        _putchar(str[i]);
+    }
+}
+
+
+/**
+ * _strcmp -  function that compares two strings.
+ * @s1: first string.
+ * @s2: second string.
+ * Return: 0.
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+        int res = 0;
+
+        while (*s1 == *s2 && *s1 != '\0')
+        {
+                s1++;
+                s2++;
+        }
+        if (s1 != s2)
+                res = *s1 - *s2;
+
+        return (res);
+}
---
+++ v2/shell.c
@@ -0,0 +1,40 @@
+#include "main.h"
+
+int main() {
+    char input[MAX_INPUT_SIZE];
+    char* tokens[MAX_NUM_TOKENS];
+    int status;
+    int i;
+    ssize_t read_size;
+    pid_t pid;
+
+    while (1) 
+    {
+        printf("#cisfun$ ");
+        
+        fflush(stdout);
+        read_size = read(STDIN_FILENO, input, MAX_INPUT_SIZE);
+        
+        if (read_size == 0) 
+            break;
+
+        /*if (_strcmp(input, "exit") == 0)
+        break;*//*for exit*/
+
+        parse_input(input, tokens, &i);
+
+        pid = fork();
+
+        if (pid <= 0) 
+        {
+            execve(tokens[0], tokens, NULL);
+            perror("./shell");
+            exit(1);
+        } 
+        else 
+            wait(&status);
+    
+    }
+
+    return 0;
+}
